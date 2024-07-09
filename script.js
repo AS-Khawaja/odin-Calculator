4b65d898868c42e2b9ffeec2b299e9d6e70f8b2b
AS Khawaja <azansaeed6@gmail.com>
2024-07-09 18:56:27 +0500

    Completed

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let operator='';
 let result=0;
 
 function evaluate(num1,num2,operator){
-    console.log({num1});
-    console.log({num2});
     let op1=parseInt(num1);
     let op2=parseInt(num2);
 
@@ -26,10 +24,15 @@ function evaluate(num1,num2,operator){
     else if(operator=='%'){
         return op1%op2;
     }
+    else if(operator=='^'){
+        return op1**op2;
+    }
 }
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
+        buttons[i].addEventListener('click', function() {
+
         let value = buttons[i].textContent;
+        document.getElementById('sc-dis').textContent=value;
         let numval=parseInt(value);
 
         if((numval>=0&&numval<=9)&&operator==='')
@@ -38,7 +41,7 @@ for (let i = 0; i < buttons.length; i++) {
             op1+=value;
         }
 
-        if(value=='+'|| value=='-' || value=='*' || value=='/' || value=='%')
+        if(value=='+'|| value=='-' || value=='*' || value=='/' || value=='%'|| value=='^')
         {
             operator=value;
 
@@ -53,13 +56,13 @@ for (let i = 0; i < buttons.length; i++) {
             op2='';
             operator='';
             result=0;
-            document.getElementById('display').innerHTML=0;
+            document.getElementById('sc-dis').innerHTML=0;
         }
         if(value=='=' && op1!='' && op2!='' && operator!=''){
             result=evaluate(op1,op2,operator);
-            document.getElementById('display').textContent=result;
-            console.log({result});
-            op1='';
+            document.getElementById('sc-dis').textContent=result;
+            let temp=result.toString();
+            op1=temp;
             op2='';
             operator='';
         }
